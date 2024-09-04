ea579e5dbb6f8affa1eebe07c201157a79da94e8
rprakashdass <rprakashdass@gmail.com>
2024-09-05 00:57:01 +0530

    create virtual env

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -10,6 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+"""
+    superuser - 
+        username : 7102
+        email : test@gmail.com
+        password : 7102
+
+"""
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
