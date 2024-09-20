605e922fce2577e3da9fffae554d9c08d12fe9c1
rprakashdass <rprakashdass@gmail.com>
2024-09-20 11:23:54 +0530

    upload static files

--- frontend/src/JS/index.js
+++
@@ -1,9 +0,0 @@
-<script>
-function showOverlay() {
-    document.getElementById('loginOverlay').style.display = 'flex'
-}
-
-function hideOverlay() {
-    document.getElementById('loginOverlay').style.display = 'none'
-}
-</script>
